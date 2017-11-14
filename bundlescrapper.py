6d846dec8b7f379be981264f9326af76de99171d
jerrymon <jerilonline@gmail.com>
2017-11-14 12:48:54 -0500

    cleanup code and readme.md

--- bundlescrapper.py
+++ bundlescrapper.py
@@ -1,43 +1,16 @@
-#! /usr/bin/env python3
-
-# webscrapper - https://www.youtube.com/watch?v=7SWVXPYZLJM
-# scrap dest - https://www.humblebundle.com/books/start-a-startup-books
-
-import sys
 import requests
 from bs4 import BeautifulSoup
 
 print('Start to scrape')
 
-# setup virtual env
-# https://www.youtube.com/watch?v=N5vscPTWKOk
-
 url = 'https://www.humblebundle.com/books/start-a-startup-books'
 
-# make an http request to the url and inspect the result of that
-# http://docs.python-requests.org/en/master/
-# use get HTTP request to get the resource
-# add requests to requirements.txt file
-# pip install -r requirements.txt
-
 resp = requests.get(url)
-print(resp)
-
-# Parsing HTML with BS4 aka Beautiful Soup
-# add bs4 to requirements.txt file
-# pip install -r requirements.txt
-# bs4 doc - https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-# changed virtualenv in File >> Settings >> Project >> Project Interpreter
-
 soup = BeautifulSoup(resp.text, 'html.parser')
 #print(soup)
 #print(soup.p)
-
-# Bundle tiers
-# dd-header-headline
 #print(soup.findAll('.dd-header-headline'))
 #print(soup.findAll('h2'))
-
 #testy = soup.findAll('h2')
 #print(testy[0])
 
--- readme.md
+++ readme.md
@@ -1,1 +1,28 @@
-"# bundlescrapper" 
+"# bundlescrapper"
+# Read me for bundle scrapper
+
+#! /usr/bin/env python3
+
+# webscrapper - https://www.youtube.com/watch?v=7SWVXPYZLJM
+# scrap dest - https://www.humblebundle.com/books/start-a-startup-books
+
+# setup virtual env
+# https://www.youtube.com/watch?v=N5vscPTWKOk
+
+# make an http request to the url and inspect the result of that
+# http://docs.python-requests.org/en/master/
+# use get HTTP request to get the resource
+# add requests to requirements.txt file
+# pip install -r requirements.txt
+
+# Parsing HTML with BS4 aka Beautiful Soup
+# add bs4 to requirements.txt file
+# pip install -r requirements.txt
+# bs4 doc - https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# changed virtualenv in File >> Settings >> Project >> Project Interpreter
+
+# Bundle tiers
+# dd-header-headline
+
+# We are capturing 'Support Charity' by mistake ..!
+
